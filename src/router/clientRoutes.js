5d6cac54a79c1c6e57d677a9ad634105ac691c9c
aguay <ItsNotMyFault_Dev@hotmail.com>
2024-05-21 19:46:03 -0400

    moving routes

--- src/router/clientRoutes.js
+++ src/router/clientRoutes.js
@@ -22,37 +22,5 @@ export const clientRoutes = [
     meta: {
       title: 'My Reservations'
     }
-  },
-  {
-    path: '/my-team',
-    name: 'myteam',
-    component: () => import('@views/Team/MyTeamView.vue'),
-    meta: {
-      title: 'My Team'
-    }
-  },
-  {
-    path: '/my-team/edit',
-    name: 'myteam-edit',
-    component: () => import('@views/Team/MyTeamEditView.vue'),
-    meta: {
-      title: 'Edit my team'
-    }
-  },
-  {
-    path: '/team-reservations',
-    name: 'teamreservations',
-    component: () => import('@views/Reservation/TeamReservationView.vue'),
-    meta: {
-      title: 'Team Reservations'
-    }
-  },
-  {
-    path: '/team-calendar',
-    name: 'teamcalendar',
-    component: () => import('@views/Team/TeamCalendarAdminView.vue'),
-    meta: {
-      title: 'Team admin calendar'
-    }
   }
 ]
--- src/router/teamRoutes.js
+++ src/router/teamRoutes.js
@@ -23,5 +23,37 @@ export const teamRoutes = [
     meta: {
       title: 'Équipe création'
     }
+  },
+  {
+    path: '/my-team',
+    name: 'myteam',
+    component: () => import('@views/Team/MyTeamView.vue'),
+    meta: {
+      title: 'My Team'
+    }
+  },
+  {
+    path: '/my-team/edit',
+    name: 'myteam-edit',
+    component: () => import('@views/Team/MyTeamEditView.vue'),
+    meta: {
+      title: 'Edit my team'
+    }
+  },
+  {
+    path: '/team-reservations',
+    name: 'teamreservations',
+    component: () => import('@views/Reservation/TeamReservationView.vue'),
+    meta: {
+      title: 'Team Reservations'
+    }
+  },
+  {
+    path: '/team-calendar',
+    name: 'teamcalendar',
+    component: () => import('@views/Team/TeamCalendarAdminView.vue'),
+    meta: {
+      title: 'Team admin calendar'
+    }
   }
 ]
