fce1d1830e63aa4402535df3478190ca813141cb
aguay <ItsNotMyFault_Dev@hotmail.com>
2024-05-21 20:03:21 -0400

    split team cards in another file

---
+++ src/components/team/TeamCards.vue
@@ -0,0 +1,111 @@
+<template>
+    <div>
+        <ActivityCard img="20230814_183252">
+            <template #title>
+                TITLE
+            </template>
+            <template #image>
+                <img alt="img" src="@/assets/images/20230817_191711.jpg" />
+            </template>
+            <template #description>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias
+                perspiciatis, quos temporibus veniam iste excepturi minus ea esse. Qui ipsam culpa non iste animi
+                aspernatur
+                numquam ipsa unde ut? Eaque.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque officiis enim, placeat dolores,
+                eveniet
+                amet
+                dolore eligendi magnam itaque veritatis nam. Aliquam sint consequatur tenetur, aspernatur ducimus
+                amet
+                unde
+                iusto.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo error ad non modi quam adipisci
+                exercitationem
+                sequi ea dicta? Optio ipsa pariatur ratione ea a molestias vero repudiandae eos delectus.
+            </template>
+        </ActivityCard>
+        <ActivityCard reverse>
+            <template #title>
+                TITLE
+            </template>
+            <template #image>
+                <img alt="img" src="@/assets/images/20240204_124524.jpg" />
+            </template>
+            <template #description>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias
+                perspiciatis, quos temporibus veniam iste excepturi minus ea esse. Qui ipsam culpa non iste animi
+                aspernatur
+                numquam ipsa unde ut? Eaque.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque officiis enim, placeat dolores,
+                eveniet
+                amet
+                dolore eligendi magnam itaque veritatis nam. Aliquam sint consequatur tenetur, aspernatur ducimus
+                amet
+                unde
+                iusto.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo error ad non modi quam adipisci
+                exercitationem
+                sequi ea dicta? Optio ipsa pariatur ratione ea a molestias vero repudiandae eos delectus.
+            </template>
+        </ActivityCard>
+        <ActivityCard>
+            <template #title>
+                TITLE
+            </template>
+            <template #image>
+                <img alt="img" src="@/assets/images/20231118_112354.jpg" />
+            </template>
+            <template #description>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias
+                perspiciatis, quos temporibus veniam iste excepturi minus ea esse. Qui ipsam culpa non iste animi
+                aspernatur
+                numquam ipsa unde ut? Eaque.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque officiis enim, placeat dolores,
+                eveniet
+                amet
+                dolore eligendi magnam itaque veritatis nam. Aliquam sint consequatur tenetur, aspernatur ducimus
+                amet
+                unde
+                iusto.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo error ad non modi quam adipisci
+                exercitationem
+                sequi ea dicta? Optio ipsa pariatur ratione ea a molestias vero repudiandae eos delectus.
+            </template>
+        </ActivityCard>
+        <ActivityCard reverse>
+            <template #title>
+                TITLE
+            </template>
+            <template #image>
+                <img alt="img" src="@/assets/images/20240308_151349.jpg" />
+            </template>
+            <template #description>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias
+                perspiciatis, quos temporibus veniam iste excepturi minus ea esse. Qui ipsam culpa non iste animi
+                aspernatur
+                numquam ipsa unde ut? Eaque.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque officiis enim, placeat dolores,
+                eveniet
+                amet
+                dolore eligendi magnam itaque veritatis nam. Aliquam sint consequatur tenetur, aspernatur ducimus
+                amet
+                unde
+                iusto.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo error ad non modi quam adipisci
+                exercitationem
+                sequi ea dicta? Optio ipsa pariatur ratione ea a molestias vero repudiandae eos delectus.
+            </template>
+        </ActivityCard>
+    </div>
+</template>
+<script>
+import ActivityCard from '@components/ActivityCard.vue'
+
+export default {
+    name: 'TeamCards',
+
+    components: {
+        ActivityCard,
+    },
+}
+</script>
--- src/views/Team/detail.vue
+++ src/views/Team/detail.vue
@@ -7,108 +7,12 @@
             </div>
         </div>
         <br>
+        <TeamCards></TeamCards>
         <ClientCalendar v-if="showCalendar" :teamName="normalizedTeamName"></ClientCalendar>
-        <div>
-            <ActivityCard img="20230814_183252">
-                <template #title>
-                    TITLE
-                </template>
-                <template #image>
-                    <img alt="img" src="@/assets/images/20230817_191711.jpg" />
-                </template>
-                <template #description>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias
-                    perspiciatis, quos temporibus veniam iste excepturi minus ea esse. Qui ipsam culpa non iste animi
-                    aspernatur
-                    numquam ipsa unde ut? Eaque.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque officiis enim, placeat dolores,
-                    eveniet
-                    amet
-                    dolore eligendi magnam itaque veritatis nam. Aliquam sint consequatur tenetur, aspernatur ducimus
-                    amet
-                    unde
-                    iusto.
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo error ad non modi quam adipisci
-                    exercitationem
-                    sequi ea dicta? Optio ipsa pariatur ratione ea a molestias vero repudiandae eos delectus.
-                </template>
-            </ActivityCard>
-            <ActivityCard reverse>
-                <template #title>
-                    TITLE
-                </template>
-                <template #image>
-                    <img alt="img" src="@/assets/images/20240204_124524.jpg" />
-                </template>
-                <template #description>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias
-                    perspiciatis, quos temporibus veniam iste excepturi minus ea esse. Qui ipsam culpa non iste animi
-                    aspernatur
-                    numquam ipsa unde ut? Eaque.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque officiis enim, placeat dolores,
-                    eveniet
-                    amet
-                    dolore eligendi magnam itaque veritatis nam. Aliquam sint consequatur tenetur, aspernatur ducimus
-                    amet
-                    unde
-                    iusto.
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo error ad non modi quam adipisci
-                    exercitationem
-                    sequi ea dicta? Optio ipsa pariatur ratione ea a molestias vero repudiandae eos delectus.
-                </template>
-            </ActivityCard>
-            <ActivityCard>
-                <template #title>
-                    TITLE
-                </template>
-                <template #image>
-                    <img alt="img" src="@/assets/images/20231118_112354.jpg" />
-                </template>
-                <template #description>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias
-                    perspiciatis, quos temporibus veniam iste excepturi minus ea esse. Qui ipsam culpa non iste animi
-                    aspernatur
-                    numquam ipsa unde ut? Eaque.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque officiis enim, placeat dolores,
-                    eveniet
-                    amet
-                    dolore eligendi magnam itaque veritatis nam. Aliquam sint consequatur tenetur, aspernatur ducimus
-                    amet
-                    unde
-                    iusto.
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo error ad non modi quam adipisci
-                    exercitationem
-                    sequi ea dicta? Optio ipsa pariatur ratione ea a molestias vero repudiandae eos delectus.
-                </template>
-            </ActivityCard>
-            <ActivityCard reverse>
-                <template #title>
-                    TITLE
-                </template>
-                <template #image>
-                    <img alt="img" src="@/assets/images/20240308_151349.jpg" />
-                </template>
-                <template #description>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias
-                    perspiciatis, quos temporibus veniam iste excepturi minus ea esse. Qui ipsam culpa non iste animi
-                    aspernatur
-                    numquam ipsa unde ut? Eaque.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque officiis enim, placeat dolores,
-                    eveniet
-                    amet
-                    dolore eligendi magnam itaque veritatis nam. Aliquam sint consequatur tenetur, aspernatur ducimus
-                    amet
-                    unde
-                    iusto.
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo error ad non modi quam adipisci
-                    exercitationem
-                    sequi ea dicta? Optio ipsa pariatur ratione ea a molestias vero repudiandae eos delectus.
-                </template>
-            </ActivityCard>
-        </div>
     </div>
 </template>
 <script>
+import TeamCards from '@components/team/TeamCards.vue'
 import ActivityCard from '@components/ActivityCard.vue'
 import ClientCalendar from '@components/calendar/ClientCalendar.vue'
 import teamServices from '@services/teamServices.js'
@@ -125,6 +29,7 @@ export default {
     },
 
     components: {
+        TeamCards,
         ActivityCard,
         ClientCalendar
     },
