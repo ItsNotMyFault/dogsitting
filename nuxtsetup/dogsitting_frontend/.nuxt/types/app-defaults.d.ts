2c3e7adb5ff1e3a1e9dfb98f831a9dc25ea4c217
Alexis <alexis_raphael_guay@hotmail.com>
2025-10-12 19:36:54 -0400

    rm cached .nuxt

--- nuxtsetup/dogsitting_frontend/.nuxt/types/app-defaults.d.ts
+++
@@ -1,7 +0,0 @@
-
-declare module '#app/defaults' {
-  type DefaultAsyncDataErrorValue = undefined
-  type DefaultAsyncDataValue = undefined
-  type DefaultErrorValue = undefined
-  type DedupeOption = 'cancel' | 'defer'
-}
